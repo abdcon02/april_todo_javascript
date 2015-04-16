79efc6c18e56e8854a6947ff4ef164321c713b92
connor abdelnoor & Tom Mertz <student@epicodus.com>
2015-04-16 11:31:28 -0700

    Setup html for todo list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,19 @@
-$(function () {
+$(function() {
 
-    $('form#new_Task').submit(function(event) {
+    $('form#newList').submit(function(event) {
         event.preventDefault();
-        var new_Task_Name = $('input#task_Name').val();
-        var new_Task_Description = $('input#task_Description').val();
 
-        var new_Task = { name: new_Task_Name, description: new_Task_Description };
+        var listOption = $('input#list_name').val();
+        $('input#list_name').val("");
 
-        $('input#task_Name').val("");
-        $('input#task_Description').val("");
+        var todo = { item: listOption,
+                     tasks: []
+                     }
 
-        $('#tasks').append('<li class="task">' + new_Task.name + '</br>' + new_Task.description + '</li>');
+        $('#list_items').append('<li>' + todo.item + '</li>');
 
-        $('.task').last().click(function() {
-            $(this).remove();
-            $('#completed').append('<li class="completed_task">' + new_Task.name + '</br>' + new_Task.description + '</li>');
-        });
+        
 
-        $('#completed').last().click(function() {
-            $(this).remove();
-        });
 
 
     });
