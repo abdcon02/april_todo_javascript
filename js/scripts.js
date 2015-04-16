65cdd1f8be257205eccf03b74ff187c0c2d48293
connor abdelnoor & Tom Mertz <student@epicodus.com>
2015-04-16 11:59:11 -0700

    Add tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 $(function() {
 
-    $('form#newList').submit(function(event) {
+  var list_array = [];
+
+  $('form#newList').submit(function(event) {
+      event.preventDefault();
+
+      var listOption = $('input#list_name').val();
+      $('input#list_name').val("");
+
+      var todo = { item: listOption,
+                   tasks: []
+                   }
+
+      $('#list_items').append('<li class="list_item">' + todo.item + '</li>');
+
+      $('.list_item').last().click(function() {
+        $('#selected_list').text(todo.item);
+        $('#add-task').show();
+      });
+
+      $('form#newTask').submit(function(event) {
         event.preventDefault();
 
-        var listOption = $('input#list_name').val();
-        $('input#list_name').val("");
-
-        var todo = { item: listOption,
-                     tasks: []
-                     }
-
-        $('#list_items').append('<li>' + todo.item + '</li>');
-
-        
-
-
-
-    });
+        var taskOption = $('#task_name').val();
+        $('#task_name').val('');
+        todo.tasks.push(taskOption);
+        $('#tasks').append("<li>" + taskOption + "</li>");
+      });
+  });
 
 
 });
